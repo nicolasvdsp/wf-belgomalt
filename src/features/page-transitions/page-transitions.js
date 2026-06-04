d977433503de73b44b4c970d19da677c562115d4
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2026-06-04 16:42:51 +0200

    feat(dialog): adds a dialog that acts like a drawer

--- src/features/page-transitions/page-transitions.js
+++ src/features/page-transitions/page-transitions.js
@@ -1081,7 +1081,7 @@ function initPageTransitions() {
     preventRunning: true,
     prevent: ({ el }) => {
       if (el?.hash && el.pathname.replace(/\/$/, '') === window.location.pathname.replace(/\/$/, '')) return true;
-      return !!el?.closest('[data-counter-add], [data-anchor-target]');
+      return !!el?.closest('[data-counter-add], [data-anchor-target], [data-dialog-open], [data-dialog-close]');
     },
     transitions: [
       { //item to detail page
